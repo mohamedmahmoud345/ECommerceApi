52a5ffa1cde399cbd1701a1c9a98b87530920d38
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-21 04:04:31 +0300

    fix: add product id property

--- Core/Entities/CartItem.cs
+++ Core/Entities/CartItem.cs
@@ -4,15 +4,17 @@ namespace Core.Entities
     public class CartItem
     {
         public Guid Id { get; private set; }
+
         public Product Product { get; private set; }
+        public Guid ProductId { get; private set; }
         public int Quantity { get; private set; }
         public DateTime DateAdded { get; private set; }
 
 
-        public CartItem(Product product, int quantity)
+        public CartItem(Guid productId, int quantity)
         {
             Id = Guid.NewGuid();
-            Product = product;
+            ProductId = productId;
             Quantity = quantity;
             DateAdded = DateTime.UtcNow;
         }
