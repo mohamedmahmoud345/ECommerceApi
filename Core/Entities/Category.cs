a6677c6ac58d03745f9f3801ce0eabfb68768aa7
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-21 04:05:38 +0300

    fix: add admin id property

--- Core/Entities/Category.cs
+++ Core/Entities/Category.cs
@@ -4,17 +4,18 @@ namespace Core.Entities
 {
     public class Category
     {
-        public Category(string name , string description)
+        public Category(string name , string description, Guid? adminId)
         {
             Name = name;
             Description = description;
+            AdminId = adminId;
         }
 
         public Guid Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
         public Admin? Admin { get; private set; }
-
+        public Guid? AdminId { get; private set; }
         public void UpdateDescription(string description) => Description = description;
         public void Rename(string newName) => Name = newName;
     }
