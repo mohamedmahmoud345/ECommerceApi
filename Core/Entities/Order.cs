c0ced476b72a49edac929316971c0a29ee961876
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-21 04:07:45 +0300

    fix: add customer id and admin id property

--- Core/Entities/Order.cs
+++ Core/Entities/Order.cs
@@ -4,12 +4,13 @@ namespace Core.Entities
 {
     public class Order
     {
-        public Order(DateTime date, OrderStatus status, Customer customer)
+        public Order(DateTime date, OrderStatus status, Guid customerId, Guid? adminId)
         {
             Id = Guid.NewGuid();
             Date = date;
             Status = status;
-            Customer = customer;
+            CustomerId = customerId;
+            AdminId = adminId;
         }
 
         public Guid Id { get; private set; }
@@ -17,9 +18,11 @@ namespace Core.Entities
         public decimal TotalAmount => _items.Sum(x => x.TotalAmount());
         public DateTime Date { get; private set; }  
         public OrderStatus Status { get; private set; }
+        public Guid CustomerId { get; private set; }
         public Customer Customer { get; private set; }
         public Admin? Admin { get; private set; }
-        
+        public Guid? AdminId { get; private set; }
+
         public IReadOnlyCollection<OrderItem> Items => _items; 
         public void AddItem(Guid productId, int quantity, decimal unitPrice)
         {
