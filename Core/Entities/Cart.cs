f090b97ffc1ed0590b4adb57a5c721ea8c192990
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-21 04:38:05 +0300

    refactor: add validation and (quantity,total amount) proprety

--- Core/Entities/Cart.cs
+++ Core/Entities/Cart.cs
@@ -5,17 +5,17 @@ namespace Core.Entities
     public class Cart
     {
         public Guid Id { get; private set; }
-        public int Quantity { get; private set; }
         public DateTime Date { get; private set; }
 
         public Guid CustomerId { get; private set; }
         public Customer Customer { get; private set; }
         private List<CartItem> _items = new List<CartItem>();
 
-        public Cart(int quantity, Guid customerId)
+        public int Quantity => _items.Sum(x => x.Quantity);
+        public decimal TotalAmount => _items.Sum(x => x.Quantity * x.Product.Price);
+        public Cart(Guid customerId)
         {
             Id = Guid.NewGuid();
-            Quantity = quantity;
             Date = DateTime.UtcNow;
             CustomerId = customerId;
         }
@@ -23,6 +23,11 @@ namespace Core.Entities
 
         public void AddItem(Product product , int quantity)
         {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+            if(quantity < 0)
+                throw new ArgumentOutOfRangeException(nameof(quantity));
+
             var item = _items.FirstOrDefault(x => x.Product.Id == product.Id);
             if(item != null)
             {
@@ -30,11 +35,13 @@ namespace Core.Entities
             }
             else
             {                
-                _items.Add(new CartItem(product, quantity));
+                _items.Add(new CartItem(product.Id, quantity));
             }
         }
         public void RemoveItem(Product product)
         {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
             var item = _items.FirstOrDefault(x => x.Product.Id == product.Id);
             if (item != null)
                 _items.Remove(item);
