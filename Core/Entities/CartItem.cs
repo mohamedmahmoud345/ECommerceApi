5f9307eaf61864183b6d787c46cbb79616655276
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-22 05:06:15 +0300

    refactor: add cart navigation property and parameterless constructor for ef core

--- Core/Entities/CartItem.cs
+++ Core/Entities/CartItem.cs
@@ -8,13 +8,17 @@ namespace Core.Entities
         public Guid ProductId { get; private set; }
         public int Quantity { get; private set; }
         public DateTime DateAdded { get; private set; }
+        public Guid CartId { get; private set; }
+        public Cart Cart { get; private set; }
 
+        private CartItem() { }
 
-        public CartItem(Product product, int quantity)
+        public CartItem(Guid cartId,Product product, int quantity)
         {
             if (quantity <= 0)
                 throw new ArgumentOutOfRangeException(nameof(quantity), "Quantity must be positive");
             Id = Guid.NewGuid();
+            CartId = cartId;
             Product = product;
             ProductId = product.Id;
             Quantity = quantity;
