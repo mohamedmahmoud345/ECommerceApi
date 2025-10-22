f1cdf0861273bbbd3d4373a49c4074b9f0061330
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-22 05:04:46 +0300

    refactor: add order and product navigation property and parameterless constructor for ef core

--- Core/Entities/OrderItem.cs
+++ Core/Entities/OrderItem.cs
@@ -15,10 +15,13 @@
                 throw new ArgumentException(nameof(UnitPrice));
             UnitPrice = unitPrice;
         }
+        private OrderItem() { }
 
         public Guid Id { get; private set; }
         public Guid OrderId { get; private set; }
+        public Order Order { get; private set; }
         public Guid ProductId { get; private set; }
+        public Product Product { get; private set; }
         public int Quantity { get; private set; }
         public decimal UnitPrice { get; private set; }
 
