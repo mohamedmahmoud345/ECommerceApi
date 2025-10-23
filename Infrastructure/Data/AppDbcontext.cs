51bfc721c15f996ca56569faad80e95a05886643
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-23 20:54:36 +0300

    add application dbcontext

---
+++ Infrastructure/Data/AppDbcontext.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbcontext : DbContext
+    {
+        public DbSet<Cart> Carts{ get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+
+        public AppDbcontext()
+        {            
+        }
+        public AppDbcontext(DbContextOptions<AppDbcontext> options) : base(options)
+        {
+        }
+    }
+}
