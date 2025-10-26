615a41b18642b6078c9b5e7f9404a8e7b6e99468
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-26 14:42:49 +0300

    add unit of work

---
+++ Application/IUnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿
+using Application.Interfaces.IRepositories;
+
+namespace Application.IUnitOfWorks
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IProductRepository Products { get; }
+        IOrderRepository Orders { get; }
+        ICartRepository Carts { get; }
+        ICustomerRepository Customers { get; }
+        ICategoryRepository Categories { get; }
+        IReviewRepository Reviews { get; }
+        IPaymentRepository Payments { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ Infrastructure/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿
+using Application.Interfaces.IRepositories;
+using Application.IUnitOfWorks;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+
+namespace Infrastructure.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context, IProductRepository products, IOrderRepository orders, ICartRepository carts, ICustomerRepository customers, ICategoryRepository categories, IReviewRepository reviews, IPaymentRepository payments)
+        {
+            _context = context;
+            Products = new ProductRepository(context);
+            Orders = new OrderRepository(context);
+            Carts = new CartRepository(context);
+            Customers = new CustomerRepository(context);
+            Categories = new CategoryRepository(context);
+            Reviews = new ReviewRepository(context);
+            Payments = new PaymentRepository(context);
+        }
+
+        public IProductRepository Products { get; }
+
+        public IOrderRepository Orders {get;}
+
+        public ICartRepository Carts {get;}
+
+        public ICustomerRepository Customers {get;}
+
+        public ICategoryRepository Categories {get;}
+
+        public IReviewRepository Reviews {get;}
+
+        public IPaymentRepository Payments {get;}
+
+        
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
