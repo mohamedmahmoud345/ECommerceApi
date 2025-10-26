3008946919cd276b6db24e9bd355d04d83eef516
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-26 20:42:52 +0300

    add cart dto

---
+++ Application/Dto/CartDto/CartDto.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Core.Entities;
+
+namespace Application.Dto.CartDto
+{
+    public class CartDto
+    {
+        public Guid Id { get;  set; }
+        public DateTime Date { get;  set; }
+        public Guid CustomerId { get;  set; }
+        public Customer Customer { get;  set; }
+        public DateTime CreatedAt { get;  set; }
+        public int Quantity { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
---
+++ Application/Dto/CartDto/ConvertExtentionMethod.cs
@@ -0,0 +1,46 @@
+﻿
+using Core.Entities;
+
+namespace Application.Dto.CartDto
+{
+    public static class ConvertExtentionMethod
+    {
+        public static CartDto ToDto(this Cart cart)
+        {
+            var cartDto = new CartDto()
+            {
+                Id = cart.Id,
+                Date = cart.Date,
+                CreatedAt = cart.CreatedAt,
+                Customer = cart.Customer,
+                CustomerId = cart.CustomerId,
+                Quantity = cart.Quantity,
+                TotalAmount = cart.TotalAmount,
+            };
+
+            return cartDto;
+        }
+        public static List<CartDto> ToDto(this List<Cart> carts)
+        {
+            var cartsDto = new List<CartDto>();
+            foreach (var cart in carts) 
+                cartsDto.Add(cart.ToDto());
+
+            return cartsDto;
+        }
+        public static Cart FromDto(this CartDto cartDto)
+        {
+            var cart = new Cart(cartDto.CustomerId);
+
+            return cart;
+        }
+        public static List<Cart> FromDto(this List<CartDto> cartsDto)
+        {
+            var carts = new List<Cart>();
+            foreach(var cart in cartsDto)
+                carts.Add(new Cart(cart.CustomerId));
+
+            return carts;
+        }
+    }
+}
