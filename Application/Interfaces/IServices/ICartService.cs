9868da8b79b83e7de72472b6f88bc3f78d4e94ad
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-26 20:41:00 +0300

    add cart interface service

--- Application/Interfaces/IServices/ICartService.cs
+++ Application/Interfaces/IServices/ICartService.cs
@@ -1,7 +1,15 @@
-﻿namespace Application.Interfaces.IServices
+﻿using Application.Dto.CartDto;
+using Core.Entities;
+
+namespace Application.Interfaces.IServices
 {
     public interface ICartService
     {
-
+        Task<CartDto?> GetByCustomerIdAsync(Guid customerId);
+        Task<bool> HasActiveCartAsync(Guid customerId);
+        Task<CartDto> GetByIdAsync(Guid id);
+        Task Update(CartDto entity);
+        Task AddAsync(CartDto entity);
+        Task DeleteAsync(Guid id);
     }
 }
