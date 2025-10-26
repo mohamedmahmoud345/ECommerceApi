a87bbd2328859cd6d3dc35e87ae1aa371f9c2b8e
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-26 14:00:34 +0300

    repositories dependency injection using static method

---
+++ Infrastructure/Dependecies/RepositoriesDependecies.cs
@@ -0,0 +1,24 @@
+﻿
+
+using Application.Interfaces.IRepositories;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Dependecies
+{
+    public static class RepositoriesDependecies
+    {
+        public static IServiceCollection AddRepositoriesDependecies(this IServiceCollection services)
+        {
+            services.AddScoped<ICartRepository, CartRepository>();
+            services.AddScoped<ICategoryRepository , CategoryRepository>();
+            services.AddScoped<ICustomerRepository , CustomerRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IPaymentRepository, PaymentRepository>();
+            services.AddScoped<IProductRepository , ProductRepository>();   
+            services.AddScoped<IReviewRepository , ReviewRepository>();
+
+            return services;
+        }
+    }
+}
