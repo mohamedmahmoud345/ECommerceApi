e9a2e927c0e8f096a6efd5faf2a7fc6013854b38
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-29 17:56:22 +0300

    add cqrs and mediator

---
+++ Application/Features/Customers/Queries/GetCustomerById/GetByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Application.Features.Customers.Response;
+using Core.Entities;
+using MediatR;
+
+namespace Application.Features.Customers.Queries.GetCustomerById
+{
+    public class GetCustomerByIdQuery : IRequest<GetCustomerByIdResponse>
+    {
+        public GetCustomerByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+        public Guid Id { get; set; }
+    }
+}
---
+++ Application/Features/Customers/Response/GetCustomerByIdResponse.cs
@@ -0,0 +1,15 @@
+﻿
+using Core.Entities;
+
+namespace Application.Features.Customers.Response
+{
+    public class GetCustomerByIdResponse
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Phone { get; private set; }
+        public string Address { get; private set; }
+
+    }
+}
