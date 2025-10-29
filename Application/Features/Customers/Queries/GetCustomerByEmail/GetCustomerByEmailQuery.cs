079d06f57a0645b90a2515a26f5a826623fff5b4
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-30 00:32:51 +0300

    add GetCustomerByEmail(query, handler , response)

---
+++ Application/Features/Customers/Queries/GetCustomerByEmail/GetCustomerByEmailQuery.cs
@@ -0,0 +1,15 @@
+﻿
+using MediatR;
+
+namespace Application.Features.Customers.Queries.GetCustomerByEmail
+{
+    public class GetCustomerByEmailQuery : IRequest<GetCustomerByEmailResponse>
+    {
+        public string Email { get; set; }
+
+        public GetCustomerByEmailQuery(string email)
+        {
+            Email = email;
+        }
+    }
+}
