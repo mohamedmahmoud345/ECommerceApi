826170d12ef5a29a15f148f3d668b45d5009d247
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-29 17:57:10 +0300

    add mapping

---
+++ Application/Mapping/Customers/CustomerProfile.cs
@@ -0,0 +1,15 @@
+﻿
+using Application.Features.Customers.Response;
+using AutoMapper;
+using Core.Entities;
+
+namespace Application.Mapping.Customers
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<Customer, GetCustomerByIdResponse>();
+        }
+    }
+}
