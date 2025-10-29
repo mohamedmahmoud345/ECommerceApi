102be87f8c6a2b06cc9639b46413507ae7d64fa2
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-29 17:55:41 +0300

    add customer controller

---
+++ Api/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Features.Customers.Queries.GetCustomerById;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CustomerController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var customer = await _mediator.Send(new GetCustomerByIdQuery(id));
+            if (customer == null)
+                return NotFound();
+            return Ok(customer);
+        }
+    }
+}
