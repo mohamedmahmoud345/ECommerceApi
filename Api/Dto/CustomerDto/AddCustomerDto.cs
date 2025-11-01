6a2632e3226a5543da140c365868bab3186acbfa
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-11-01 23:00:53 +0200

    add Dto

---
+++ Api/Dto/CustomerDto/AddCustomerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Dto.CustomerDto
+{
+    public class AddCustomerDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Api/Dto/CustomerDto/UpdateCustomerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Dto.CustomerDto
+{
+    public class UpdateCustomerDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
