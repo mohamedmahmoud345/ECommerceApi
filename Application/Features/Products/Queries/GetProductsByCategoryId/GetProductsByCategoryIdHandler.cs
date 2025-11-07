2833e6506871d11ac0483b4feb030d855a2c7742
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-11-07 22:08:42 +0200

    add GetProductsByCategoryId query

---
+++ Application/Features/Products/Queries/GetProductsByCategoryId/GetProductsByCategoryIdHandler.cs
@@ -0,0 +1,30 @@
+﻿
+using Application.IUnitOfWorks;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Products.Queries.GetProductsByCategoryId
+{
+    public class GetProductsByCategoryIdHandler : IRequestHandler<GetProductsByCategoryIdQuery, List<GetProductsByCategoryIdResponse>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetProductsByCategoryIdHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GetProductsByCategoryIdResponse?>> Handle(GetProductsByCategoryIdQuery request, CancellationToken cancellationToken)
+        {
+            var isIdValid = await _unitOfWork.Categories.GetByIdAsync(request.Id);
+            if (isIdValid == null)
+                return null;
+
+            var products = await _unitOfWork.Products.GetByCategoryIdAsync(request.Id);
+
+            return _mapper.Map<List<GetProductsByCategoryIdResponse>>(products);
+        }
+    }
+}
---
+++ Application/Features/Products/Queries/GetProductsByCategoryId/GetProductsByCategoryIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Application.Features.Products.Queries.GetProductsByCategoryId
+{
+    public class GetProductsByCategoryIdQuery : IRequest<List<GetProductsByCategoryIdResponse>>
+    {
+        public Guid Id { get; init; }
+
+        public GetProductsByCategoryIdQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
