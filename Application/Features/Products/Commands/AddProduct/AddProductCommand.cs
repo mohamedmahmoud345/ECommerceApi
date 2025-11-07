89d7e267e165fcebd48092c529d5e317abd91feb
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-11-07 22:08:08 +0200

    add addProductcommand

---
+++ Application/Features/Products/Commands/AddProduct/AddProductCommand.cs
@@ -0,0 +1,15 @@
+﻿
+using MediatR;
+
+namespace Application.Features.Products.Commands.AddProduct
+{
+    public class AddProductCommand : IRequest<AddProductResponse>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
---
+++ Application/Features/Products/Commands/AddProduct/AddProductHandler.cs
@@ -0,0 +1,32 @@
+﻿
+using Application.IUnitOfWorks;
+using AutoMapper;
+using Core.Entities;
+using MediatR;
+
+namespace Application.Features.Products.Commands.AddProduct
+{
+    public class AddProductHandler : IRequestHandler<AddProductCommand, AddProductResponse>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public AddProductHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<AddProductResponse?> Handle(AddProductCommand request, CancellationToken cancellationToken)
+        {
+            var validCategory = await _unitOfWork.Categories.GetByIdAsync(request.CategoryId);
+            if (validCategory == null)
+                return null;            
+
+            var product = new Product(request.Name, request.Description,request.ImageUrl ,request.Price, request.StockQuantity, request.CategoryId);
+            await _unitOfWork.Products.AddAsync(product);
+            await _unitOfWork.SaveChangesAsync();
+            return _mapper.Map<AddProductResponse>(product);
+        }
+    }
+}
