209b99ae79d13b6c231bd08277b0589e79078cab
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-12-18 16:18:42 +0200

    add cart response

--- Application/Features/Cart/Common/CartItemResponse.cs
+++ Application/Features/Cart/Common/CartItemResponse.cs
@@ -1,13 +1,14 @@
-﻿namespace Application.Features.Cart.Common;
-
-public class CartItemResponse
+﻿namespace Application.Features.Cart.Common
 {
-    public Guid Id { get; set; }
-    public Guid ProductId { get; set; }
-    public string ProductName { get; set; }
-    public string ProductImageUrl { get; set; }
-    public decimal Price { get; set; }
-    public int Quantity { get; set; }
-    public decimal SubTotal { get; set; }
-    public DateTime DateAdded { get; set; }
+    public class CartItemResponse
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductImageUrl { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateAdded { get; set; }
+        public decimal SubTotal { get; set; }
+    }
 }
