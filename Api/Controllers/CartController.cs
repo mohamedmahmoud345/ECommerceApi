acdb619d2fd62978a95cf53912e196457da24b33
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-12-18 16:19:30 +0200

    add cart controller and dto

---
+++ Api/Controllers/CartController.cs
@@ -0,0 +1,39 @@
+﻿using Api.Dto.Cart;
+using Application.Features.Cart.Commands.AddItemToCart;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CartController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddItemDto itemDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var command = new AddItemToCartCommand()
+            {
+                CustomerId = itemDto.CustomerId,
+                ProductId = itemDto.ProductId,
+                Quantity = itemDto.Quantity
+            };
+
+            var result = await _mediator.Send(command);
+            if (result == null)
+                return BadRequest();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Api/Dto/Cart/AddItemDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Api.Dto.Cart
+{
+    public record AddItemDto(Guid CustomerId,
+        Guid ProductId, int Quantity);
+}
