a68d13f4da1c5800be9d2f21235d4d7a046b4074
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-12-21 15:29:39 +0200

    add CartItems DbSet

--- Infrastructure/Data/AppDbcontext.cs
+++ Infrastructure/Data/AppDbcontext.cs
@@ -5,16 +5,16 @@ namespace Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Cart> Carts{ get; set; }
+        public DbSet<Cart> Carts { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Payment> Payments { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Review> Reviews { get; set; }
-
+        public DbSet<CartItem> CartItems { get; set; }
         public AppDbContext()
-        {            
+        {
         }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
