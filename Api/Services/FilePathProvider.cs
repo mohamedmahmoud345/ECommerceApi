1fcecac7df9fe37e4257aaf7f94e58ba97ff3698
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-12-26 18:53:33 +0200

    add services folder and implement IFilePathProvider

---
+++ Api/Services/FilePathProvider.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces.Services;
+
+namespace Api.Services
+{
+    public class FilePathProvider : IFilePathProvider
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public FilePathProvider(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public string WebRootPath()
+        {
+            return _env.WebRootPath;
+        }
+    }
+}
