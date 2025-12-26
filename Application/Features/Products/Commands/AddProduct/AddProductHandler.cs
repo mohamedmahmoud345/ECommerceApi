37914305c3ebed8ae4dc9f3def8dd8b5937ade74
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-12-26 18:47:10 +0200

    refactor and inject ifileStorageService to handle image

--- Application/Features/Products/Commands/AddProduct/AddProductHandler.cs
+++ Application/Features/Products/Commands/AddProduct/AddProductHandler.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using Application.Interfaces.Services;
 using Application.IUnitOfWorks;
 using AutoMapper;
 using Core.Entities;
@@ -6,27 +6,50 @@ using MediatR;
 
 namespace Application.Features.Products.Commands.AddProduct
 {
-    public class AddProductHandler : IRequestHandler<AddProductCommand, AddProductResponse>
+    public class AddProductHandler : IRequestHandler<AddProductCommand, AddProductResponse?>
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-
-        public AddProductHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        private readonly IFileStorageService _fileService;
+        public AddProductHandler(IUnitOfWork unitOfWork, IMapper mapper, IFileStorageService fileService)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _fileService = fileService;
         }
 
         public async Task<AddProductResponse?> Handle(AddProductCommand request, CancellationToken cancellationToken)
         {
-            var validCategory = await _unitOfWork.Categories.GetByIdAsync(request.CategoryId);
-            if (validCategory == null)
-                return null;            
+            string imagePath = null;
+            try
+            {
+                var category = await _unitOfWork.Categories.GetByIdAsync(request.CategoryId);
+                if (category is null)
+                    return null;
 
-            var product = new Product(request.Name, request.Description,request.ImageUrl ,request.Price, request.StockQuantity, request.CategoryId);
-            await _unitOfWork.Products.AddAsync(product);
-            await _unitOfWork.SaveChangesAsync();
-            return _mapper.Map<AddProductResponse>(product);
+                imagePath = await _fileService.SaveAsync(
+                        request.ImageStream,
+                        request.ImageName,
+                        "Products"
+                    );
+
+                request.ImageStream.Dispose();
+
+                var product = new Product(request.Name, request.Description, imagePath, request.Price, request.StockQuantity, request.CategoryId);
+
+                await _unitOfWork.Products.AddAsync(product);
+                await _unitOfWork.SaveChangesAsync();
+
+
+                return _mapper.Map<AddProductResponse>(product);
+            }
+            catch (Exception ex)
+            {
+                if (imagePath != null)
+                    await _fileService.DeleteAsync(imagePath);
+
+                throw;
+            }
         }
     }
 }
