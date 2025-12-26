5ae8e57234f0d6f5e14e8f059eecc0749706d004
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-12-26 18:50:34 +0200

    remove imageUrl to isolation the image with other data

--- Application/Features/Products/Commands/UpdateProduct/UpdateProductCommand.cs
+++ Application/Features/Products/Commands/UpdateProduct/UpdateProductCommand.cs
@@ -8,7 +8,6 @@ namespace Application.Features.Products.Commands.UpdateProduct
         public Guid Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public string? ImageUrl { get; set; }
         public decimal? Price { get; set; }
         public int? StockQuantity { get; set; }
         public Guid? CategoryId { get; set; }
