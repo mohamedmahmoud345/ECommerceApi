ec96ec96667538c85205ef2bac8a13fbe0eb81ff
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-12-26 18:56:01 +0200

    add IfilePathProvider and IFileStorage services

---
+++ Application/Interfaces/Services/IFileStorageService.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Interfaces.Services
+{
+    public interface IFileStorageService
+    {
+        Task<string> SaveAsync(Stream stream, string fileName, string folder);
+        Task DeleteAsync(string filePath);
+    }
+}
