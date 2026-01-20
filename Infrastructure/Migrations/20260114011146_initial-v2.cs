aaec130f4d0046304d1d0ce05bd68c2a5f89c3d6
M.Mahmoud <mohamed987456mm20@gmail.com>
2026-01-21 00:29:47 +0200

    feat: add price property to cartItem and update migration for database schema

---
+++ Infrastructure/Migrations/20260114011146_initial-v2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialv2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "CartItems",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "CartItems");
+        }
+    }
+}
