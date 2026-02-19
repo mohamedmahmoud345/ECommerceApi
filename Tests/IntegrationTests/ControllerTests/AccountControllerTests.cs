ec1c2d24c352d897ef2cff954978356d0f4b7673
M.Mahmoud <mohamed987456mm20@gmail.com>
2026-02-19 05:35:20 +0200

    feat: add login tests for invalid and valid credentials in AccountControllerTests

--- Tests/IntegrationTests/ControllerTests/AccountControllerTests.cs
+++ Tests/IntegrationTests/ControllerTests/AccountControllerTests.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
 using Api.Dto.Account;
+using Application.Features.Account;
 using Xunit.Abstractions;
 
 namespace IntegrationTests.ControllerTests
@@ -35,19 +36,58 @@ namespace IntegrationTests.ControllerTests
         {
             // Given
             var request =
-                        new RegisterDto("ahmed", "ahmed42@gmail.com", "SecurePass123!", "01201834567", "beni-suef");
+                        new RegisterDto("ahmed", "alkjasd42@gmail.com", "SecurePass123!", "01201834567", "beni-suef");
 
             // When
             var response = await _client.PostAsJsonAsync("/api/account/register", request);
             var response2 = await _client.PostAsJsonAsync("/api/account/register", request);
 
-            // Debug output
-            var responseBody = await response.Content.ReadAsStringAsync();
-            var responseBody2 = await response2.Content.ReadAsStringAsync();
-
             // Then
-            Assert.Equal(HttpStatusCode.Created, response.StatusCode); // First should succeed
             Assert.Equal(HttpStatusCode.BadRequest, response2.StatusCode); // Second should fail
+        }
+        [Fact]
+        public async Task Login_WithInValidPassword_return400BadRequest()
+        {
+            // Given
+            var register = new RegisterDto("ahmed", "try-test1@gmail.com", "SecurePass123!", "01201834567", "beni-suef");
+            var request = new LoginDto("try-test1@gmail.com", "SecuePass123!");
+            // When
+            var response = await _client.PostAsJsonAsync("api/account/register", register);
+            var response2 = await _client.PostAsJsonAsync("api/account/login", request);
+            // Then
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.Equal(HttpStatusCode.BadRequest, response2.StatusCode);
+        }
+        [Fact]
+        public async Task Login_WithInValidEmail_return400BadRequest()
+        {
+            // Given
+            var register = new RegisterDto("abdo", "trytest2@gmail.com", "SecurePass123!", "01201834567", "beni-suef");
+            var request = new LoginDto("trdy-test2@gmail.com", "SecurePass123!");
+            // When
+            var response = await _client.PostAsJsonAsync("api/account/register", register);
+            var response2 = await _client.PostAsJsonAsync("api/account/login", request);
+            // Then
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.Equal(HttpStatusCode.BadRequest, response2.StatusCode);
+        }
+        [Fact]
+        public async Task Login_WithValidData_return200Ok()
+        {
+            // Given
+            var register = new RegisterDto("mohmd", "try-test3@gmail.com", "SecurePass123!", "01201834567", "beni-suef");
+            var request = new LoginDto("try-test3@gmail.com", "SecurePass123!");
+            // When
+            var response = await _client.PostAsJsonAsync("api/account/register", register);
+            var response2 = await _client.PostAsJsonAsync("api/account/login", request);
+            // Then
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, response2.StatusCode);
+
+            var loginResult = await response2.Content.ReadFromJsonAsync<AuthResponse>();
+            Assert.NotNull(loginResult);
+            Assert.NotNull(loginResult.Token);
+            Assert.NotEmpty(loginResult.Token);
         }
     }
 }
